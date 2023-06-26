4e8cb8359d46c959413cedc684de7914fa84c175
Alex Rocha <rocha.amc@gmail.com>
2023-06-25 22:41:35 -0700

    Integrated Socket.io

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-//App.js
 const express = require('express');
 const app = express();
-//Socket.io has to use the http server
-const server = require('http').Server(app);
+const server = require('http').Server(app)
 
-//Express View Engine for Handlebars
+//Socket.io
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  // This file will be read on new socket connections
+  require('./sockets/chat.js')(io, socket);
+})
+
 const exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
+app.set('views', './views');
+//Establish your public folder
+app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index.handlebars');
 })
-  >
-  server.listen('3000', () => {
-    console.log('Server listening on Port 3000');
-  })
+
+server.listen('3030', () => {
+  console.log('Server listening on Port 3030');
+})
